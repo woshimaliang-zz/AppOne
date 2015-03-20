e4f8e7fea89c3946b47f9bd7cdb970ee1d83ed96
woshimaliang <liangma@msn.com>
2015-03-19 18:01:13 -0700

    add lines to shape; add attributes to label; support AFNetwork and standard NSUrlConnection

---
+++ AppOne/ShapeMainView.h
@@ -0,0 +1,13 @@
+//
+//  ShapeMainView.h
+//  AppOne
+//
+//  Created by Ma, Liang (Liang) on 3/17/15.
+//  Copyright (c) 2015 Ma, Liang (Liang). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShapeMainView : UIView
+
+@end
---
+++ AppOne/ShapeMainView.m
@@ -0,0 +1,13 @@
+//
+//  ShapeMainView.m
+//  AppOne
+//
+//  Created by Ma, Liang (Liang) on 3/17/15.
+//  Copyright (c) 2015 Ma, Liang (Liang). All rights reserved.
+//
+
+#import "ShapeMainView.h"
+
+@implementation ShapeMainView
+
+@end
